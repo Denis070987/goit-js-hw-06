078928c99ef104b654207b48635ff25ac57cea75
Denis070987 <deniskoms@gmail.com>
2022-10-24 17:28:03 +0300

    add task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+const currentСolor = document.querySelector('.color');
+const clickMe = document.querySelector('.change-color');
+clickMe.addEventListener('click', () => {
+  let changeColor = [getRandomHexColor()];
+  document.body.style.backgroundColor = changeColor;
+  currentСolor.textContent = changeColor;
+  console.log(currentСolor);
+})
+
+
